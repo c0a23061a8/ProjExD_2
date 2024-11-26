4e0a38aef62498bad316ebb48a750df60b36dfe1
c0a23061 <c0a23061a8@edu.teu.ac.jp>
2024-11-26 14:56:07 +0900

    練習4 こうかとんと爆弾の衝突判定の実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -44,6 +44,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        # こうかとんが爆弾と衝突したらゲームオーバー
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return 
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
